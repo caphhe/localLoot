44fb130d46754c10a9ba01b85dd680f089d33bb8
Paul Redetzky <p_redetzky@gmx.de>
2020-03-21 21:36:00 +0100

    added second camera for move inspection

---
+++ LocalLoot/Assets/Scripts/Camera/CameraTransitionOnStopMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+[RequireComponent(typeof(Rigidbody))]
+public class CameraTransitionOnStopMove : MonoBehaviour
+{
+    [SerializeField]
+    CinemachineVirtualCamera cm1, cm2;
+    [SerializeField]
+    float requiredVelocity;
+    Rigidbody rb;
+    private bool initialVelocityMet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initialVelocityMet = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!initialVelocityMet &&
+            rb.velocity.magnitude > requiredVelocity)
+            initialVelocityMet = true;
+
+        if (rb.velocity.magnitude < .05f && initialVelocityMet)
+        {
+            cm1.gameObject.SetActive(false);
+            cm2.gameObject.SetActive(true);
+        }
+    }
+
+}
