c452b6d36000535f0fbba42dc80efc8cd1c31d09
Paul Redetzky <p_redetzky@gmx.de>
2020-03-21 22:57:13 +0100

    added scene updatable

---
+++ LocalLoot/Assets/Scripts/SceneManagerUpdater.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerUpdater : StateMachineBehaviour
+{
+    public List<MonoBehaviour> behavioursToUpdate;
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        
+    }
+}
