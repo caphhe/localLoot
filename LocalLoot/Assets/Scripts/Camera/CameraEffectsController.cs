1a9ed586bbd7c247d9c9ebcbe2aa0f6e52460981
Paul Redetzky <p_redetzky@gmx.de>
2020-03-21 21:16:34 +0100

    added timeline & camera shake on ground hit

---
+++ LocalLoot/Assets/Scripts/Camera/CameraEffectsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CameraEffectsController : MonoBehaviour
+{
+    static CameraEffectsController instance;
+    Animator animator;
+    private void Awake()
+    {
+        instance = this;
+        animator = GetComponent<Animator>();
+    }
+
+
+    public static void Shake()
+    {
+        instance.animator.Play("Shake");
+    }
+
+}
