2574c4c36f72ca9488c8535934f8533cf3a6c138
caphhe <caphhe1994@gmail.com>
2020-03-21 14:10:17 +0100

    rotation snapping

--- LocalLoot/Assets/Scripts/Wheel/WheelManager.cs
+++ LocalLoot/Assets/Scripts/Wheel/WheelManager.cs
@@ -8,14 +8,24 @@ public class WheelManager : MonoBehaviour
 	[SerializeField] private int visibleBoxes = 5;
 	[SerializeField] private float distanceInDegrees = 5f;
 	[SerializeField] private float radius = 5f;
+	[SerializeField] private float speedDampening = 0.9f;
+	[SerializeField] private float userSpeedMod = 0.25f;
+	[SerializeField] private float snapForce = 1f;
+
 
 
 	[SerializeField] private float speed = 1f;
+	private float userSpeed = 0;
 	private List<GameObject> boxes = new List<GameObject>();
+	private Rigidbody rb; 
+
+	private Vector2 lastTouchPos = new Vector2 (-1, -1);
 	
 	// Start is called before the first frame update
     void Start()
     {
+		rb = gameObject.GetComponent<Rigidbody>();
+		rb.centerOfMass = gameObject.transform.position;
         for(int i = 0; i < visibleBoxes; i++)
 		{
 			AddBox();
@@ -25,6 +35,9 @@ public class WheelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		Touchinput();
+		CalcSpeed();
+		//rb.AddTorque(new Vector3(userSpeed *100, 0, 0), ForceMode.Acceleration);
 		gameObject.transform.Rotate(Vector3.right, speed);
 		if (Mathf.Abs(transform.rotation.eulerAngles.x % distanceInDegrees) < Mathf.Abs(speed))
 		{
@@ -61,5 +74,64 @@ public class WheelManager : MonoBehaviour
 	{
 		float angle = index * distanceInDegrees - 2 * distanceInDegrees;
 		return gameObject.transform.position - (Quaternion.Euler(angle, 0, 0) * new Vector3(0, 0, radius));
+	}
+
+	private void CalcSpeed ()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			speed = userSpeed * userSpeedMod;
+		} else
+		{
+			
+			float distFromRestPos = (transform.rotation.eulerAngles.x - distanceInDegrees/2) % distanceInDegrees - distanceInDegrees / 2;
+			float distFromRestPercent = distFromRestPos / (distanceInDegrees / 2);
+
+			speed += distFromRestPercent * snapForce; 
+
+			speed *= speedDampening;
+
+			if (Mathf.Abs(distFromRestPos) < 0.1f && Mathf.Abs(speed) < 0.01f)
+			{
+				speed = 0;
+			}
+		}
+	}
+
+
+	private void Touchinput ()
+	{
+		if (Input.touchCount == 1)
+		{
+			if (Input.GetTouch(0).phase == TouchPhase.Began)
+			{
+				lastTouchPos = Input.GetTouch(0).position;
+			} else
+			{
+				userSpeed = (Input.GetTouch(0).position.y - lastTouchPos.y) / Screen.height;
+				lastTouchPos = Input.GetTouch(0).position;
+			}
+		} else
+		{
+			userSpeed = 0;
+		}
+#if UNITY_EDITOR
+		if (Input.GetMouseButton(0))
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				lastTouchPos = Input.mousePosition;
+			}
+			else
+			{
+				userSpeed = (Input.mousePosition.y - lastTouchPos.y) / Screen.height;
+				lastTouchPos = Input.mousePosition;
+			}
+		}
+		else
+		{
+			userSpeed = 0;
+		}
+#endif
 	}
 }
