542678b5bcff03ce6a97276976f88e5edc4aeb2b
Paul Redetzky <p_redetzky@gmx.de>
2020-03-21 15:45:21 +0100

    wheel working

--- LocalLoot/Assets/Scripts/Wheel/WheelManager.cs
+++ LocalLoot/Assets/Scripts/Wheel/WheelManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class WheelManager : MonoBehaviour
 {
+    public enum WheelState
+    {
+        Idle,
+        Spinning,
+        Interacted,
+        ReachingDestination,
+
+    }
+    
 	[SerializeField] private GameObject boxPrefab = null;
 	[SerializeField] private int visibleBoxes = 5;
 	[SerializeField] private float distanceInDegrees = 5f;
 	[SerializeField] private float radius = 5f;
-	[SerializeField] private float speedDampening = 0.9f;
+    float distanceRad;
+    [SerializeField] private float speedDampening = 0.9f;
 	[SerializeField] private float userSpeedMod = 0.25f;
 	[SerializeField] private float snapForce = 1f;
 
-
+    WheelState state;
 
 	[SerializeField] private float speed = 1f;
 	private float userSpeed = 0;
@@ -30,19 +40,20 @@ public class WheelManager : MonoBehaviour
 		{
 			AddBox();
 		}
+        distanceRad = Mathf.Deg2Rad * distanceInDegrees;
     }
 
     // Update is called once per frame
     void Update()
     {
 		Touchinput();
-		CalcSpeed();
+		CalcSpeed ();
 		//rb.AddTorque(new Vector3(userSpeed *100, 0, 0), ForceMode.Acceleration);
-		gameObject.transform.Rotate(Vector3.right, speed);
-		if (Mathf.Abs(transform.rotation.eulerAngles.x % distanceInDegrees) < Mathf.Abs(speed))
-		{
-			SwapBoxPosition(speed > 0);
-		}
+		gameObject.transform.Rotate(Vector3.right, speed * Time.deltaTime);
+		//if (Mathf.Abs(transform.rotation.eulerAngles.x % distanceInDegrees) < Mathf.Abs(speed))
+		//{
+		//	SwapBoxPosition(speed > 0);
+		//}
     }
 
 	private void AddBox ()
@@ -81,27 +92,64 @@ public class WheelManager : MonoBehaviour
 		if (Input.GetMouseButton(0))
 		{
 			speed = userSpeed * userSpeedMod;
-		} else
-		{
-			
-			float distFromRestPos = (transform.rotation.eulerAngles.x - distanceInDegrees/2) % distanceInDegrees - distanceInDegrees / 2;
-			float distFromRestPercent = distFromRestPos / (distanceInDegrees / 2);
-
-			speed += distFromRestPercent * snapForce; 
-
-			speed *= speedDampening;
-
-			if (Mathf.Abs(distFromRestPos) < 0.1f && Mathf.Abs(speed) < 0.01f)
-			{
-				speed = 0;
-			}
 		}
+            else
+		{
+            //         float angleSegment = (transform.rotation.eulerAngles.x - distanceInDegrees / 2);
+            //         float modAngles =  angleSegment % distanceInDegrees;
+            //         Debug.Log(modAngles);
+            //         float distFromRestPos = modAngles - distanceInDegrees / 2;
+            //float distFromRestPercent = distFromRestPos / (distanceInDegrees / 2);
+
+            //speed += distFromRestPercent * snapForce; 
+
+            //speed *= speedDampening;
+
+
+
+            float angleSegment = (Mathf.Deg2Rad * (transform.rotation.eulerAngles.x - (distanceInDegrees / 2.0f))) % distanceRad;
+            float sinDist = Mathf.Sin(transform.rotation.x * Mathf.Deg2Rad) - Mathf.Sin(angleSegment);
+            float roundedSpeed = Mathf.Round(100.0f * (Mathf.Asin(sinDist)));
+            roundedSpeed = roundedSpeed / 100.0f;
+            roundedSpeed *= Mathf.Rad2Deg;
+            if (Mathf.Abs(roundedSpeed) > 0.0f)
+            {
+                speed += snapForce / roundedSpeed * Time.deltaTime;
+            }
+            else
+            {
+                speed *= 1 / snapForce;
+            }
+            speed *= speedDampening;
+            speed *= 100.0f;
+            speed = Mathf.Round(speed);
+            speed /= 100.0f;
+        }
 	}
 
 
 	private void Touchinput ()
 	{
-		if (Input.touchCount == 1)
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                lastTouchPos = Input.mousePosition;
+            }
+            else
+            {
+                userSpeed = (Input.mousePosition.y - lastTouchPos.y) / Screen.height;
+                lastTouchPos = Input.mousePosition;
+            }
+        }
+        else
+        {
+            userSpeed = 0;
+        }
+#elif UNITY_ANDROID
+
+        if (Input.touchCount == 1)
 		{
 			if (Input.GetTouch(0).phase == TouchPhase.Began)
 			{
@@ -115,23 +163,7 @@ public class WheelManager : MonoBehaviour
 		{
 			userSpeed = 0;
 		}
-#if UNITY_EDITOR
-		if (Input.GetMouseButton(0))
-		{
-			if (Input.GetMouseButtonDown(0))
-			{
-				lastTouchPos = Input.mousePosition;
-			}
-			else
-			{
-				userSpeed = (Input.mousePosition.y - lastTouchPos.y) / Screen.height;
-				lastTouchPos = Input.mousePosition;
-			}
-		}
-		else
-		{
-			userSpeed = 0;
-		}
 #endif
-	}
+    }
+
 }
