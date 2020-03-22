77a7c4e6ceacdcab8cb09f4f9f0312dbf05ab6bb
Paul Redetzky <p_redetzky@gmx.de>
2020-03-22 16:38:11 +0100

    added state update after lootbox ground hit

---
+++ LocalLoot/Assets/Scripts/BoxHitGround.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BoxHitGround : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        FindObjectOfType<SceneManager>().gameObject.GetComponent<Animator>().SetBool("LootBoxOnGround", true);
+    }
+}
